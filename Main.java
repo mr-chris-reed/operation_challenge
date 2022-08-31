b78330b1b5c2685469cd92c30c66562cd7f719f9
Christopher Reed <84865108+mr-chris-reed@users.noreply.github.com>
2022-08-31 08:25:53 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+/* You will write a console program that asks the user for two integers.  The user will supply two integers.  Your program should perform the following operations on the two integers: + , -, *, /, and %.  Your program should neatly and clearly output calculations in a meaningful way to the user
+*/
+
+class Main {
+  public static void main(String[] args) {
+    
+  }
+}
